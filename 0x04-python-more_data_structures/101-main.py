850e9ce74a0e251c82527c551a8221d90ac8289d
jeffsonsenior <kinyuajef@gmail.com>
2024-01-11 02:39:39 +0000

    14. Squared by using map

--- 0x04-python-more_data_structures/101-main.py
+++ 0x04-python-more_data_structures/101-main.py
@@ -3,9 +3,9 @@ square_matrix_map = \
     __import__('101-square_matrix_map').square_matrix_map
 
 matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
+[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]
 ]
 
 new_matrix = square_matrix_map(matrix)
